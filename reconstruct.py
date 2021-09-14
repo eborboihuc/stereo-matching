8592c3c07899efad92bd8b4679625e7066cb2b53
eborboihuc <eborboihuc@gmail.com>
2021-09-14 10:10:20 +0800

    [refactor] Format file with yapf

--- reconstruct.py
+++ reconstruct.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+
 import cv2
-import json
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from scipy.spatial.transform import Rotation as R
 
 from sgbm import SGBMatcher
 
@@ -15,24 +16,44 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Semi-Global Matching')
     parser.add_argument('left_image_name')
     parser.add_argument('right_image_name')
-    parser.add_argument('-M', default=1, type=int, help='The sparsity of y-axis of image')
-    parser.add_argument('-N', default=1, type=int, help='The sparsity of x-axis of image')
+    parser.add_argument('-M',
+                        default=1,
+                        type=int,
+                        help='The sparsity of y-axis of image')
+    parser.add_argument('-N',
+                        default=1,
+                        type=int,
+                        help='The sparsity of x-axis of image')
 
     parser.add_argument('-f', '--focal_length', default=500, type=float)
-    parser.add_argument('-d', '--distance_between_cameras', default=10, type=float)
+    parser.add_argument('-d',
+                        '--distance_between_cameras',
+                        default=10,
+                        type=float)
 
-    parser.add_argument('--num_disparities', default=80, type=int,
-            help='The max_disp in use, must be dividable by 16. default: 16.')
-    parser.add_argument('--block_size', default=5, type=int,
-            help='Block size in calculation. default: 5.')
-    parser.add_argument('--window_size', default=15, choices=[3, 5, 7, 15],
-            type=int,
-            help='How large a search window is. default: 5.')
+    parser.add_argument(
+        '--num_disparities',
+        default=80,
+        type=int,
+        help='The max_disp in use, must be dividable by 16. default: 16.')
+    parser.add_argument('--block_size',
+                        default=5,
+                        type=int,
+                        help='Block size in calculation. default: 5.')
+    parser.add_argument('--window_size',
+                        default=15,
+                        choices=[3, 5, 7, 15],
+                        type=int,
+                        help='How large a search window is. default: 5.')
 
-    parser.add_argument('--show_disp_map', action='store_true', default=False, 
-            help='Visualize disparity results')
-    parser.add_argument('--show_depth_map', action='store_true', default=False, 
-            help='Visualize depth results')
+    parser.add_argument('--show_disp_map',
+                        action='store_true',
+                        default=False,
+                        help='Visualize disparity results')
+    parser.add_argument('--show_depth_map',
+                        action='store_true',
+                        default=False,
+                        help='Visualize depth results')
     args = parser.parse_args()
 
     assert args.num_disparities % 16 == 0, "The max_disp must be dividable by 16."
@@ -41,8 +62,16 @@ def parse_args():
 
 
 class Reconstructor():
-
-    def __init__(self, img_l, img_r, disp, focal_length, baseline, show_disp_map=False, show_depth_map=False, trans_speed=50, angle_speed=np.pi/180):
+    def __init__(self,
+                 img_l,
+                 img_r,
+                 disp,
+                 focal_length,
+                 baseline,
+                 show_disp_map=False,
+                 show_depth_map=False,
+                 trans_speed=50,
+                 angle_speed=np.pi / 180):
         self.img_l = img_l
         self.img_r = img_r
         self.disp = disp
@@ -52,22 +81,15 @@ class Reconstructor():
         self.show_depth_map = show_depth_map
 
         self.height, self.width, _ = img_l.shape
-        self.q = np.array([
-            [1, 0, 0, -self.width/2],
-            [0, 1, 0, -self.height/2],
-            [0, 0, 0, focal_length],
-            [0, 0, -1/baseline, 0]
-        ])
-        self.k = np.array([
-            [focal_length, 0, self.width/2],
-            [0, focal_length, self.height/2],
-            [0, 0, 1]
-        ])
+        self.q = np.array([[1, 0, 0, -self.width / 2],
+                           [0, 1, 0, -self.height / 2],
+                           [0, 0, 0, focal_length], [0, 0, -1 / baseline, 0]])
+        self.k = np.array([[focal_length, 0, self.width / 2],
+                           [0, focal_length, self.height / 2], [0, 0, 1]])
         self.dist_coeff = np.zeros((4, 1))
 
         self.r = np.eye(3)
         self.t = np.array([0, 0, -100.0])
-
 
         self.angles = {  # x, y, z
             'w': (angle_speed, 0, 0),
@@ -86,8 +108,17 @@ class Reconstructor():
             'i': (0, 0, trans_speed),
             'k': (0, 0, -trans_speed),
         }
-    
+
         self.points, self.colors = self._calc_point_cloud()
+
+        self.position = (20, 40)
+        self.font_face = cv2.FONT_HERSHEY_SIMPLEX
+        self.font_scale = 0.5
+        self.thickness = 1
+        text_size, _ = cv2.getTextSize("text", self.font_face, self.font_scale,
+                                       self.thickness)
+        self.line_width = text_size[0]
+        self.line_height = text_size[1] + 5
 
     def visualize(self):
         r = self.r
@@ -109,7 +140,7 @@ class Reconstructor():
                 t = self.rotate(t, self.angles[ch])
                 self._view(r, t)
 
-            if ch == 'h': # reset
+            if ch == 'h':  # reset
                 self._view(self.r, self.t)
 
             elif ch == '\x1b':  # esc
@@ -142,20 +173,18 @@ class Reconstructor():
     def translate(arr, trans):
         return arr + np.array(trans)
 
-
     @staticmethod
     def remove_invalid(disp_arr, points, colors):
         # TODO: Add nearclip to filter out the wrong projection
-        mask = (
-            (disp_arr > disp_arr.min()) &
-            np.all(~np.isnan(points), axis=1) &
-            np.all(~np.isinf(points), axis=1)
-        )
+        mask = ((disp_arr > disp_arr.min()) & np.all(~np.isnan(points), axis=1)
+                & np.all(~np.isinf(points), axis=1))
         return points[mask], colors[mask]
 
-
     def _calc_point_cloud(self):
-        points = cv2.reprojectImageTo3D(self.disp, self.q, handleMissingValues=True).reshape(-1, 3)
+        points = cv2.reprojectImageTo3D(self.disp,
+                                        self.q,
+                                        handleMissingValues=True).reshape(
+                                            -1, 3)
         if self.show_disp_map:
             colors = self.disp.reshape(-1, 1)
             COL = MplColorHelper('viridis', colors.min(), colors.max())
@@ -168,16 +197,13 @@ class Reconstructor():
             colors = self.img_l.reshape(-1, 3)
         return self.remove_invalid(self.disp.reshape(-1), points, colors)
 
-
     def _project_points(self, r, t):
-        projected, _ = cv2.projectPoints(self.points, r, t, self.k, self.dist_coeff)
+        projected, _ = cv2.projectPoints(self.points, r, t, self.k,
+                                         self.dist_coeff)
         xy = projected.reshape(-1, 2).astype(np.int)
-        mask = (
-            (0 <= xy[:, 0]) & (xy[:, 0] < self.width) &
-            (0 <= xy[:, 1]) & (xy[:, 1] < self.height)
-        )
+        mask = ((0 <= xy[:, 0]) & (xy[:, 0] < self.width) & (0 <= xy[:, 1]) &
+                (xy[:, 1] < self.height))
         return xy[mask], self.colors[mask]
-
 
     def _calc_projected_image(self, r, t):
         xy, cm = self._project_points(r, t)
@@ -205,11 +231,11 @@ class MplColorHelper:
         self.scalarMap = cm.ScalarMappable(norm=self.norm, cmap=self.cmap)
 
     def get_rgb(self, val, dtype='uint8'):
-        out = self.scalarMap.to_rgba(val)[:,:,:3]
+        out = self.scalarMap.to_rgba(val)[:, :, :3]
         if dtype == 'float':
             return out
         else:
-            return np.clip(out*255, 0, 255).astype(np.uint8)
+            return np.clip(out * 255, 0, 255).astype(np.uint8)
 
 
 def imread(path, channel=-1):
@@ -218,7 +244,7 @@ def imread(path, channel=-1):
         if channel ==  0: load image with 1 channel (grey)
         if channel >=  1: load image with 3 channel (RGB) 
     """
-    rgb = cv2.imread(path, channel) # Load image with original dimension
+    rgb = cv2.imread(path, channel)  # Load image with original dimension
     assert rgb is not None, "Image {} is None".format(path)
     assert rgb.ndim in [2, 3], "Image {} has ndim {}".format(path, rgb.ndim)
     #rgb = cv2.cvtColor(rgb,cv2.COLOR_BGR2RGB) if rgb.ndim == 3 else rgb
@@ -238,14 +264,16 @@ def read_depth(path):
         valid(u,v) = I(u,v)>0;
     """
     depth = imread(path).astype(np.float32) / 256.0
-    assert depth.ndim == 2, "Depth {} has more ndim ({}) than 2".format(path, depth.ndim)
+    assert depth.ndim == 2, f"Depth from {path} has more ndim ({depth.ndim}) than 2Î"
     return depth
 
 
 def triangulation(disp):
     # Get depth from disp
-    depth = 0.54 * 721.5377 / disp
-    depth[disp<=1e-4] = 0
+    baseline = 0.54
+    focal_length = 721.5377
+    depth = baseline * focal_length / disp
+    depth[disp <= 1e-4] = 0
     depth = 255 * (depth - depth.min()) / depth.max()
     print("Min {}, MAX {}".format(depth.min(), depth.max()))
     return depth.astype(np.uint8)
@@ -258,10 +286,9 @@ def main():
     img_r = imread(args.right_image_name, 1)
 
     # Init SGBM
-    sgm = SGBMatcher(
-            num_disparities=args.num_disparities,
-            block_size=args.block_size,
-            window_size=args.window_size)
+    sgm = SGBMatcher(num_disparities=args.num_disparities,
+                     block_size=args.block_size,
+                     window_size=args.window_size)
 
     sgm.show_param()
 
@@ -270,18 +297,16 @@ def main():
 
     # Sparsify the computated points
     disp_ = np.zeros_like(disp)
-    disp_[::args.M, ::args.N] = disp[::args.M,::args.N]
+    disp_[::args.M, ::args.N] = disp[::args.M, ::args.N]
 
     # Init Visualizer
-    rec = Reconstructor(
-            img_l=img_l, 
-            img_r=img_r, 
-            disp=disp, 
-            focal_length=args.focal_length, 
-            baseline=args.distance_between_cameras,
-            show_disp_map=args.show_disp_map,
-            show_depth_map=args.show_depth_map
-            )
+    rec = Reconstructor(img_l=img_l,
+                        img_r=img_r,
+                        disp=disp,
+                        focal_length=args.focal_length,
+                        baseline=args.distance_between_cameras,
+                        show_disp_map=args.show_disp_map,
+                        show_depth_map=args.show_depth_map)
 
     rec.visualize()
 
--- sgbm.py
+++ sgbm.py
@@ -10,13 +10,20 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Semi-Global Matching')
     parser.add_argument('left_image_name')
     parser.add_argument('right_image_name')
-    parser.add_argument('--num_disparities', default=80, type=int,
-            help='The max_disp in use, must be dividable by 16. default: 16.')
-    parser.add_argument('--block_size', default=5, type=int,
-            help='Block size in calculation. default: 5.')
-    parser.add_argument('--window_size', default=15, choices=[3, 5, 7, 15],
-            type=int,
-            help='How large a search window is. default: 5.')
+    parser.add_argument(
+        '--num_disparities',
+        default=80,
+        type=int,
+        help='The max_disp in use, must be dividable by 16. default: 16.')
+    parser.add_argument('--block_size',
+                        default=5,
+                        type=int,
+                        help='Block size in calculation. default: 5.')
+    parser.add_argument('--window_size',
+                        default=15,
+                        choices=[3, 5, 7, 15],
+                        type=int,
+                        help='How large a search window is. default: 5.')
     args = parser.parse_args()
 
     assert args.num_disparities % 16 == 0, "The max_disp must be dividable by 16."
@@ -25,48 +32,47 @@ def parse_args():
 
 
 class SGBMatcher():
-
     def __init__(self,
-            min_disparity=0,
-            num_disparities=16,
-            block_size=5,
-            window_size=3,
-            disp12_max_diff=1,
-            uniqueness_ratio=15,
-            speckle_window_size=0,
-            speckle_range=2,
-            pre_filter_cap=63,
-            mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY):
+                 min_disparity=0,
+                 num_disparities=16,
+                 block_size=5,
+                 window_size=3,
+                 disp12_max_diff=1,
+                 uniqueness_ratio=15,
+                 speckle_window_size=0,
+                 speckle_range=2,
+                 pre_filter_cap=63,
+                 mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY):
 
         # SGBM Parameters
         # http://answers.opencv.org/question/182049/pythonstereo-disparity-quality-problems/?answer=183650#post-id-183650
         # window_size: default 3; 5 Works nicely
-        #              7 for SGBM reduced size image; 
+        #              7 for SGBM reduced size image;
         #              15 for SGBM full size image (1300px and above)
         # num_disparity: max_disp has to be dividable by 16 f. E. HH 192, 256
 
-        p1 = 8 * 3 * window_size ** 2
-        p2 = 32 * 3 * window_size ** 2
+        p1 = 8 * 3 * window_size**2
+        p2 = 32 * 3 * window_size**2
         self.param = {
-            'minDisparity' : min_disparity,
-            'numDisparities' : num_disparities,
-            'blockSize' : block_size,
-            'windowSize' : window_size,
-            'P1' : p1,
-            'P2' : p2,
-            'disp12MaxDiff' : disp12_max_diff,
-            'uniquenessRatio' : uniqueness_ratio,
-            'speckleWindowSize' : speckle_window_size,
-            'speckleRange' : speckle_range,
-            'preFilterCap' : pre_filter_cap,
-            'mode' : mode
-            }
+            'minDisparity': min_disparity,
+            'numDisparities': num_disparities,
+            'blockSize': block_size,
+            'windowSize': window_size,
+            'P1': p1,
+            'P2': p2,
+            'disp12MaxDiff': disp12_max_diff,
+            'uniquenessRatio': uniqueness_ratio,
+            'speckleWindowSize': speckle_window_size,
+            'speckleRange': speckle_range,
+            'preFilterCap': pre_filter_cap,
+            'mode': mode
+        }
 
         param = self.param.copy()
         del param['windowSize']
 
         self.left_matcher = cv2.StereoSGBM_create(**param)
-    
+
     def show_param(self):
         print(json.dumps(self.param, indent=4, sort_keys=True))
 
@@ -76,17 +82,16 @@ class SGBMatcher():
         self.display(img_l, img_r, disp_l_img)
 
     def compute(self, img_l, img_r):
-        return self.left_matcher.compute(img_l, img_r).astype(np.float32) / 16.0
+        return self.left_matcher.compute(img_l, img_r).astype(
+            np.float32) / 16.0
 
     def normalize(self, disp):
         disp_img = disp.copy()
-        cv2.normalize(
-                src=disp_img, 
-                dst=disp_img, 
-                beta=0, 
-                alpha=255, 
-                norm_type=cv2.NORM_MINMAX
-                )
+        cv2.normalize(src=disp_img,
+                      dst=disp_img,
+                      beta=0,
+                      alpha=255,
+                      norm_type=cv2.NORM_MINMAX)
         return np.uint8(disp_img)
 
     def display(self, img_l, img_r, disp):
@@ -102,7 +107,7 @@ class SGBMatcher():
 
 
 def imread(img_name):
-    img =  cv2.imread(img_name, -1)
+    img = cv2.imread(img_name, -1)
     assert img is not None, "{} is not found!".format(img_name)
 
     return img
@@ -111,10 +116,9 @@ def imread(img_name):
 def main():
     args = parse_args()
 
-    sgm = SGBMatcher(
-            num_disparities=args.num_disparities,
-            block_size=args.block_size,
-            window_size=args.window_size)
+    sgm = SGBMatcher(num_disparities=args.num_disparities,
+                     block_size=args.block_size,
+                     window_size=args.window_size)
 
     sgm.show_param()
 
